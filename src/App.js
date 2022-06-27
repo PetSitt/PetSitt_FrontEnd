65da06e47003cdf846f27b767e250eea7232a354
younhoso <thdbsgh3443@naver.com>
2022-06-27 23:10:43 +0900

    API 호출 회원가입 및 비번찾기 부분 세팅

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
-import './App.css';
+import { useQuery } from "react-query";
+import {apis} from './store/api'
 
 function App() {
+  const {isLoading, data} = useQuery('queryKey', apis.get, {
+    staleTime: 10000
+  });
+  console.log(isLoading)
+  console.log(data)
+
   return (
     <div className="App">
       
--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import { QueryClient, QueryClientProvider } from "react-query";
 import App from './App';
+const queryClient = new QueryClient();
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <QueryClientProvider client={queryClient}>
     <App />
-  </React.StrictMode>
+  </QueryClientProvider>
 );
