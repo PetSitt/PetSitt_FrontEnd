c1572fa558d4e95426cd0eef79aa526c2c123df5
younhoso <thdbsgh3443@naver.com>
2022-06-28 19:49:15 +0900

    button 컴포넌트 추가

---
+++ src/elements/Button.js
@@ -0,0 +1,35 @@
+import styled, {css} from "styled-components";
+
+const Button = ({_width, _height, _color, _bgColor, _borderRadius, _fontSize, _lineHeight, _padding, children}) => {
+	const styles = {_width, _height, _color, _bgColor, _borderRadius, _fontSize, _lineHeight, _padding, children};
+
+	return (
+		<BtnBx {...styles}></BtnBx>
+	);
+}
+
+Button.defaultProps = {
+	_width: "63px",
+	_height: "33px",
+	_color : "#fff",
+	_bgColor : "#000",
+	_borderRadius: "0.4rem",
+	
+}
+
+const BtnBx = styled.button`
+	${({theme}) => {
+		const {fontWeight} = theme;
+		return css `
+			width: ${props => props._width};
+			height: ${props => props._height};
+			font-size: ${props => props._fontSize};
+			font-weight: ${fontWeight.regular};
+			color: ${props => props._color};
+			background-color: ${props => props._bgColor};
+			border-radius: ${props => props._borderRadius};
+		`
+	}}
+`
+
+export default Button;
--- src/page/Home.js
+++ src/page/Home.js
@@ -1,6 +1,7 @@
 import { useQuery } from "react-query";
 import {apis} from '../store/api'
 import Input from '../elements/Input';
+import Button from "../elements/Button";
 
 function Home() {
   // const {isLoading, data} = useQuery('queryKey', apis.get);
@@ -8,6 +9,7 @@ function Home() {
   return (
     <div className="home">
       <Input _placeholder={"아이디(이메일)"} _required={"required"} />
+      <Button _color={"#fff"}>버튼</Button>
     </div>
   );
 }
