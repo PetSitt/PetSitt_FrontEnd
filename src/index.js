fb72f19fed8f2c031b4cf75c5586cc471e439d52
younhoso <thdbsgh3443@naver.com>
2022-06-28 09:19:21 +0900

    충돌 해결

--- src/index.js
+++ src/index.js
@@ -1,14 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-// import './index.css';
-import App from './App';
-import {BrowserRouter} from 'react-router-dom';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
 import { QueryClient, QueryClientProvider } from "react-query";
 import App from './App';
 const queryClient = new QueryClient();
@@ -19,3 +10,4 @@ root.render(
     <App />
   </QueryClientProvider>
 );
+
