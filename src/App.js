f8079146068405d489b16f3a868da6c8970f7f4b
hayeoooon <kimma0514@gmail.com>
2022-06-28 09:06:49 +0900

    map 테스트

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
-import './App.css';
+// import './App.css';
+import {Switch, Route} from 'react-router-dom';
+import MapIndex from './page/map/MapIndex';
 
 function App() {
   return (
     <div className="App">
-      
+      <Switch>
+        <Route path="/map" component={MapIndex} />
+      </Switch>
     </div>
   );
 }
