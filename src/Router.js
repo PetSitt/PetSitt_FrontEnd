5dcd41e917630276e55c96535febc5accd260afd
hayeoooon <kimma0514@gmail.com>
2022-07-06 09:44:46 +0900

    Test: 시터 상세페이지 데이터 연결

--- src/Router.js
+++ src/Router.js
@@ -2,9 +2,9 @@ import React from "react";
 import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom'
 // page
 import Home from './page/Home';
+import Detail from './page/Detail';
 import Signup from './page/Signup';
 import MapIndex from './page/MapIndex';
-import MapIndex2 from './page/MapIndex2';
 import Login from './page/Login';
 import SearchAddress from './page/SearchAddress';
 import PwFind from "./page/PwFind";
@@ -18,12 +18,12 @@ const Router = () => {
 			<Switch>
 				<Route path="/" component={Home} exact />
 				<Route path="/map" component={MapIndex} />
+				<Route path="/detail/:id" component={Detail} />
 				<Route path='/signup' component={Signup} />
 				<Route path='/pwfind' component={PwFind} />
 				<Route path='/mypage' component={Mypage} exact />
 				<Route path='/mypage/myprofile' component={Myprofile}/>
 				<Route path='/pwchange/:id' component={PwChange}/>
-				<Route path="/map2" component={MapIndex2} />{/*이 route는 테스트 끝나면 지울게요!*/}
 				<Route path="/login" component={Login}></Route>
 				<Route path="/search" component={SearchAddress}></Route>
 				<Redirect from='*' to='/' />
