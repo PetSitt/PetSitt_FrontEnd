c371ddca90bee919f00e940ae2d62bc727cbec9d
이정민 <ljeongmin0908@gmail.com>
2022-07-12 23:14:59 +0900

    update. 인풋 엘리먼트 업데이트

--- src/elements/InputBox.js
+++ src/elements/InputBox.js
@@ -1,38 +1,17 @@
 import styled from "styled-components";
 
 const InputBox = ({
-  _label,
-  _type,
-  _ref,
-  _placeholder,
-  _name,
-  _onChange,
-  _value,
+  children
 }) => {
   return (
     <InputWrap>
-      <label>{_label}</label>
-      <input
-        type={_type}
-        {... _ref ? `ref=${_ref}` : null}
-        {... _name ? `name=${_name}` : null}
-        placeholder={_placeholder}
-        onChange={_onChange}
-        value={_value}
-      />
+      {children}
     </InputWrap>
   );
 };
 
 InputBox.defaultProps = {
   _label: "라벨",
-  _type: "text",
-  _ref: "ref",
-  _placeholder: "미리보기 내용",
-  _alert: "알림 내용",
-  _name: "이름",
-  _onChange: () => {},
-  _value: "",
 };
 
 const InputWrap = styled.div`
@@ -47,6 +26,9 @@ const InputWrap = styled.div`
   & input {
     border-bottom: 1px solid rgba(120, 120, 120, 0.2);
     padding: 9px 0px;
+    :focus {
+      border-bottom: 1px solid #FC9215;
+    }
     ::placeholder {
       color: rgba(120, 120, 120, 0.6);
       font-size: 16px;
