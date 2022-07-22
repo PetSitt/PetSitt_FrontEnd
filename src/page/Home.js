122bbd525f84342209b085b2a30b8e69ae11f27b
hayeoooon <kimma0514@gmail.com>
2022-07-22 20:22:52 +0900

    Style: 메인 툴팁 css 추가

--- src/page/Home.js
+++ src/page/Home.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 import { Cookies } from "react-cookie";
 import { useQuery, useMutation, useQueryClient } from "react-query";
 import { DateObject,Calendar } from "react-multi-date-picker";
-import styled from 'styled-components';
+import styled, {keyframes} from 'styled-components';
 import { apis } from "../store/api";
 
 import Modal from "../elements/Modal";
@@ -50,6 +50,7 @@ function Home() {
 	const datesTransformed= useRef(null);
 	const [sitterCardShow, setSitterCardShow] = useState({display: false, index: null});
 	const categoryClicked = useRef(false);
+	const showTooltip = useRef(0);
 
 	const getSittersList = (queriesData, category) => {
 		const _queriesData = {...queriesData};
@@ -96,10 +97,13 @@ function Home() {
 		}
 	}
 
+	console.log(dates?.length, addressInfo,'dates, addressInfo')
 	useEffect(()=>{
 		if(dates?.length && addressInfo){
 			setQueriesData({searchDate: dates, region_2depth_name: addressInfo.region_2depth_name, x: addressInfo.x, y: addressInfo.y})
 		}
+		setSearched(true);
+		showTooltip.current++;
 	}, [dates, addressInfo])
 
 	// 로그인 여부 확인하는 api
@@ -111,6 +115,7 @@ function Home() {
 		},
 		onError: (data) => {
 			console.log(data, 'auth api 실패');
+			localStorage.removeItem('accessToken');
 			localStorage.removeItem('userName');
 			localStorage.removeItem('userEmail');
 		},
@@ -206,9 +211,7 @@ function Home() {
 		const fullHeight = window.innerHeight;
 		const filterHeight = filterAreaRef.current.clientHeight;
 		setMapHeight(fullHeight - filterHeight - 74);
-		return()=>{
-			setDefaultSearch(false);
-		}
+		showTooltip.current++;
 	},[])
 	useEffect(()=>{
 		if(categoryClicked.current){
@@ -261,50 +264,46 @@ function Home() {
 			<button type="button" onClick={getLocation} ref={getLocationButtonRef} style={{position: 'absolute', left: 0, top: 0, width: 0, height: 0}}></button>
 			<IndexPage>
 				<FilterArea ref={filterAreaRef}>
-					<div style={{position: 'relative'}}>
-						<div className="inputBox">
-							<input type="text" placeholder="구, 동을 검색해주세요. (예: 강남구 논현동)" value={addressInfo?.address_name && addressInfo?.address_name} onClick={()=>{setIframeDisplay(true); setDatepickerDisplay(false)}} readOnly/>
-							<i className="ic-search"></i>
+					<div className="searchWrap" style={{position: 'relative'}}>
+						<div style={{position: 'relative'}}>
+							<div className="inputBox">
+								<input type="text" placeholder="구, 동을 검색해주세요. (예: 강남구 논현동)" value={addressInfo?.address_name && addressInfo?.address_name} onClick={()=>{setIframeDisplay(true); setDatepickerDisplay(false)}} readOnly/>
+								<i className="ic-search"></i>
+							</div>
+							{
+								iframeDisplay && (
+									<AddressWrap style={{margin: '0 -1px'}}>
+										<SearchAddress setAddressInfo={setAddressInfo} iframeDisplay={iframeDisplay} setIframeDisplay={setIframeDisplay}/>
+										<StyledButton _title="닫기" _margin="0" _onClick={()=>setIframeDisplay(false)}/>
+									</AddressWrap>
+								)
+							}
 						</div>
-						{
-							iframeDisplay && (
-								<AddressWrap style={{margin: '0 -1px'}}>
-									<SearchAddress setAddressInfo={setAddressInfo} iframeDisplay={iframeDisplay} setIframeDisplay={setIframeDisplay}/>
-									<StyledButton _title="닫기" _margin="0" _onClick={()=>setIframeDisplay(false)}/>
-								</AddressWrap>
-							)
-						}
-					</div>
-					<div style={{position: 'relative', zIndex: 2}}>
-						<div className="inputBox date">
-							<input type="text" placeholder="날짜를 선택해주세요." value={datesTransformed.current?.length > 0 ? datesTransformed.current : ''} onClick={()=>{setDatepickerDisplay(true); setIframeDisplay(false)}} readOnly/>
-							<i className="ic-calendar"></i>
+						<div style={{position: 'relative', zIndex: 2}}>
+							<div className="inputBox date">
+								<input type="text" placeholder="날짜를 선택해주세요." value={datesTransformed.current?.length > 0 ? datesTransformed.current : ''} onClick={()=>{setDatepickerDisplay(true); setIframeDisplay(false)}} readOnly/>
+								<i className="ic-calendar"></i>
+							</div>
+							<DatepickerWrap style={{display: datepickerDisplay === true ? 'block' : 'none', position: 'absolute', left: '-1px', right: '-1px', top: '100%'}}>
+								<Calendar
+									ref={datepickerRef}
+									onChange={setDate}
+									multiple={true}
+									format="YYYY/MM/DD"
+									minDate={new Date()}
+									maxDate={new Date(today.year + 1, today.month.number, today.day)}
+									shadow={false}
+									weekDays={weekDays}
+									months={months}
+									style={{
+										borderRadius: 0,
+									}}
+								/>
+								<StyledButton _title="날짜 적용" _margin="0" _onClick={()=>{setDatepickerDisplay(false); setDatesFormat()}}/>
+							</DatepickerWrap>
 						</div>
-						<DatepickerWrap style={{display: datepickerDisplay === true ? 'block' : 'none', position: 'absolute', left: '-1px', right: '-1px', top: '100%'}}>
-							<Calendar
-								ref={datepickerRef}
-								onChange={setDate}
-								multiple={true}
-								format="YYYY/MM/DD"
-								minDate={new Date()}
-								maxDate={new Date(today.year + 1, today.month.number, today.day)}
-								shadow={false}
-								weekDays={weekDays}
-              	months={months}
-								style={{
-									borderRadius: 0,
-								}}
-							/>
-							<StyledButton _title="날짜 적용" _margin="0" _onClick={()=>{setDatepickerDisplay(false); setDatesFormat()}}/>
-						</DatepickerWrap>
+						<Tooltip className={showTooltip.current === 0 ? 'aniClass' : ''}>장소와 날짜 모두 선택해주세요.</Tooltip>
 					</div>
-					<StyledButton _onClick={()=>{
-						if(addressInfo &&  dates?.length > 0){
-							setSearched(true);
-						}else{
-							setModalDisplay(true);
-						}
-					}} _title="검색하기" _margin="20px 0 0"/>
 					<Categories>
 						<ul>
 						{categories.map((v, i) => {
@@ -312,6 +311,7 @@ function Home() {
 								<li key={i}>
 									<label>
 										<input type="checkbox" onChange={(e) => {
+											showTooltip.current = 1;
 											categoryClicked.current = true;
 											if(categoryClicked.current){
 												if(e.target.checked){ 
@@ -440,7 +440,7 @@ function Home() {
 		</div>
 		<Modal _alert={true} _display={modalDisplay} confirmOnClick={()=>setModalDisplay(false)}>
 			<div className="text_area">
-				<h3>장소와 날짜를 모두 선택해주세요.</h3>
+				<p>장소와 날짜를 모두 선택해주세요.</p>
 			</div>
 		</Modal>
 		</>
@@ -509,6 +509,9 @@ const Buttons = styled.div`
 }
 `
 const FilterArea = styled.div`
+.searchWrap{
+	z-index: 2;
+}
 .inputBox{
 	position: relative;
 	input{
@@ -549,6 +552,9 @@ const FilterArea = styled.div`
 		border-radius: 6px;
 		overflow: hidden;
 		border: 1px solid rgba(120,120,120,.4);
+		button{
+			border-radius: 0;
+		}
 	}
 	&.date{
 		max-width: 51%;
@@ -776,5 +782,40 @@ const MapArea = styled.div`
 		}
 	}
 `
-
+const tooltipAnimation = keyframes`
+	0%{
+		opacity: 1;
+	}
+	95%{
+		opacity: 1;
+	}
+	100%{
+		opacity: 0;
+	}
+`
+const Tooltip = styled.p`
+	position: absolute;
+	left: 0;
+	top: calc(100% + 16px);
+	display: inline-block;
+	background-color: rgba(25,25,25,.9);
+	border-radius: 6px;
+	padding: 0 12px;
+	line-height: 36px;
+	font-size: 14px;
+	color: #fff;
+	animation: ${tooltipAnimation} 5s forwards;
+	&:before{
+		position: absolute;
+		left: 50%;
+		top: -9px;
+		width: 0;
+		height: 0;
+		border: 7px solid rgba(25,25,25,.9);
+		border-top: 2px solid transparent;
+		border-left: 5px solid transparent;
+		border-right: 5px solid transparent;
+		content: '';
+	}
+`
 export default Home;
